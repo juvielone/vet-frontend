fbc71a17ff696cda83b84221059ade67859c4996
Juvielone Lagos <62714904+juvielone@users.noreply.github.com>
2022-09-19 02:22:39 +0800

    Updated links

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 import Header from "../components/Header";
+import { Link, useNavigate } from "react-router-dom";
 import paw from "../img/paw.svg";
 import doggo from "../img/dog-hero.png";
 
@@ -20,18 +21,18 @@ const Homepage = () => {
                 eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
               </p>
 
-              <a
-                href="/register"
+              <Link
+                to="/register"
                 className="btn btn-register btn-primary btn-lg mt-4 btn-link"
               >
                 <img src={paw} width="45" height="41" className="paw" />
                 Register
-              </a>
+              </Link>
 
-              <a href="/login" className="btn btn-link btn-login mt-4">
+              <Link to="/login" className="btn btn-link btn-login mt-4">
                 <i class="bi bi-person-fill me-1"></i>
                 Login
-              </a>
+              </Link>
             </div>
           </div>
 
